5a7ba77fe9e9623a5421370e36ece44eba1f5ad6
Youkrid <135244267+Youkrid@users.noreply.github.com>
2023-06-09 11:07:13 +0200

    Update heat_time_independente.jl
    The code function indeed the numerical solution overlaps the exact solution. Now, I will find the errors of interpolation.

--- heat_time_independente.jl
+++ heat_time_independente.jl
@@ -9,28 +9,30 @@ function heat(N)
     tc= 0.95 #thermal_conductivity
     l= 80 #length
     c = ( tc / (sh * d))^(1/2)
-    dx = l/ (N - 1 ) #N is the number of points without boundary so N+2 points in total 
+    dx = l/ (N ) #N-1 is the number of points without boundarys
 
 #____________________________Datas are okay maybe dx is the problem for the rest__________________________________________________
 
     # Matrix A from Au = B
-    A = zeros(N, N)
-    for i in 1:N
+    A = zeros(N+1, N+1)
+    A[1,1] = 1
+    A[N+1,N+1] = 1
+    for i in 2:N
         A[i, i] = 2 * c^2/ dx^2
         if i > 1
             A[i, i-1] = -c^2 / dx^2
         end
-        if i < N
+        if i < N+1
             A[i, i+1] = -c^2 / dx^2
         end
-
     end
+    
 #__________________________Matrix A print is okay____________________________________________________
 
 
 
     # Vector B from Au = B (Boundary)
-    b = zeros(N)
+    b = zeros(N+1)
     b[1] = 10
     b[end] = 50
 
@@ -54,10 +56,10 @@ end
 
 function exact_solution(N)
     l= 80#length
-    dx = l/ (N - 1) #N is the number of points without boundary so N+2 points in total 
-    u_exact = zeros(N)
+    dx = l/ (N) #N-1  is the number of points without boundarys
+    u_exact = zeros(N+1)
     x = 0
-    for i in 1:N
+    for i in 1:N+1
         u_exact[i] = 0.5 * x + 10
         x = x + dx     
     end
@@ -71,11 +73,11 @@ end
 function curbes(N)
 
     l = 80.0
-    dx = l/ (N - 1)
+    dx = l/ (N )
     exact = exact_solution(N)
     modele = heat(N)
     X = [i for i in 0:dx:l]
     plot(X, [exact,modele])
     #print(length(exact), length(modele), length(X))
-
+    #print(X)
 end 
