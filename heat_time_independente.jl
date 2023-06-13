5967beef94b46fa9f6609de5d53cafc9ac8ad612
Youkrid <135244267+Youkrid@users.noreply.github.com>
2023-06-13 11:50:58 +0200

    heat_time_independente.jl
    Resolution of the heat equation with source 1d time independente. With the build of the Matrix A 8 times faster than before.

--- heat_time_independente.jl
+++ heat_time_independente.jl
@@ -2,61 +2,32 @@ using Plots
 using SparseArrays
 using LinearAlgebra
 
-function heat(N)
+#-----------------------------------------------------------------------------SOURCE----------------------------------------------------------------------------------------------
 
-    d= 8.92 #density 
-    sh= 0.092 #specific_heat
-    tc= 0.95 #thermal_conductivity
-    l= 80 #length
-    c = ( tc / (sh * d))^(1/2)
-    dx = l/ (N ) #N is the number of points without boundary so N+2 points in total 
+function source_function(x)
+    X0 = 20.0
+    σ = 0.8
+    A = 10.
+    return  A .* exp.(.-((x .- X0).^2)./σ^2)  
+end
 
-#____________________________Datas are okay maybe dx is the problem for the rest__________________________________________________
+function source(N)
+#Parameter
+    l = 80.
+    dx = l/N
+#Mesh
+    X_vect = Vector(0:dx:l)
+    #print(X_vect) 
+# Function source
+    f = source_function(X_vect)
+    #plot(X_vect, f)
+    return f
+end
 
-    # Matrix A from Au = B
-    A = zeros(N+1, N+1)
-    A[1,1] = 1
-    A[N+1,N+1] = 1
-    for i in 2:N
-        A[i, i] = 2 * c^2/ dx^2
-        if i > 1
-            A[i, i-1] = -c^2 / dx^2
-        end
-        if i < N+1
-            A[i, i+1] = -c^2 / dx^2
-        end
-    end
-    
-#__________________________Matrix A print is okay____________________________________________________
-
-
-
-    # Vector B from Au = B (Boundary)
-    b = zeros(N+1)
-    b[1] = 10
-    b[end] = 50
-
-
-#_________________________Vector b print is okay__________________________________________________________________
-    
-    # Solving of the Linear System
-    u = A \ b
- 
-#_________________________Problem in this part __________________________________________________________________________
-    
-    
-    return u
-
-end 
-
-
-
-
-
-
+#----------------------------------------------------------------------Exact solution without SOURCE ------------------------------------------------------------------------------
 function exact_solution(N)
-    l= 80#length
-    dx = l/ (N) #N is the number of points without boundary so N+2 points in total 
+    l= 80 #length
+    dx = l/ N
     u_exact = zeros(N+1)
     x = 0
     for i in 1:N+1
@@ -66,31 +37,69 @@ function exact_solution(N)
     return u_exact
 end
 
+#-------------------------------------------------------------------Build the Matrix A for the linear problem----------------------------------------------------------------------------
 
+function matrix(n) #8 times faster than before (calculate with @btime)
+# Parameter for a copper bar 
+    d= 8.92 #density 
+    sh= 0.092 #specific_heat
+    tc= 0.95 #thermal_conductivity
+    l= 80 #length
+    c = ( tc / (sh * d))^(1/2)
+    dx = l/ n  
+# Build of the tridiagonal Matrix
+    A = spdiagm(-1 =>  -(c^2 / dx^2)* ones(n), 0 => 2 * (c^2 / dx^2) * ones(n+1), 1 => - (c^2 / dx^2) * ones(n)) 
+    A[1,1] = 1
+    A[1,2] = 0
+    A[n+1,n+1] = 1 
+    A[n+1,n] = 0
+    return A
+end
 
+#---------------------------------------------------------------Main function to solve heat equation time independente -----------------------------------------------------------------
+function heat(N)
+#Parameter of the bar 
+    l= 80 #length
+    dx = l/ N  
+# Build of the matrix A
+    A = matrix(N)
+    #print(A)
+# Vector B from Au = B (Boundary)
+    b = zeros(N+1)
+    b[1] = 10
+    b[end] = 50
+    #print(b)
+# Vector S for the source power
+    X_vect = Vector(0:dx:l)
+    S = source_function(X_vect) 
+    #print(S)
+# Solving of the Linear System
+    u = A \ (b+S)   
+    #plot(X_vect, u)
+    return u 
+end 
+
+#------------------------------------------------------------------------------------------Show error and plots -------------------------------------------------------------------------------------
 function error(N)
-
     l = 80.0
     dx = l/ (N)
     exact = exact_solution(N)
     modele = heat(N)
-    err = zeros(N+1)
-
+    erreur = zeros(N+1)
     for i in 1:N+1
-        err[i] = abs(exact[i] - modele[i])
+        erreur[i] = abs(exact[i] - modele[i])
     end
     X = [i for i in 0:dx:l]
-    plot(X, err)
+    plot(X, erreur)
     #print(X)
-    #print(err)
+    #print(erreur)
 end
 
 
 
 function curbes(N)
-
     l = 80.0
-    dx = l/ (N )
+    dx = l/ N 
     exact = exact_solution(N)
     modele = heat(N)
     X = [i for i in 0:dx:l]
@@ -98,3 +107,7 @@ function curbes(N)
     #print(length(exact), length(modele), length(X))
     #print(X)
 end 
+
+
+
+
